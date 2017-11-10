fb036e9fcead6126124e025102fc116c75b8dc50
Julian Wachholz <julian@wachholz.ch>
2017-11-10 14:42:03 +0100

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,6 +5,7 @@ alias g git
 alias l 'less --tabs=4 -FRSX'
 alias dj django-admin
 alias bc 'bc -l'
+alias gcm 'git checkout master'
 complete --command g --wraps git
 complete --command dj --wraps django-admin
 complete --command trizen --wraps pacman
@@ -13,9 +14,6 @@ function fish_greeting
   date
 end
 
-#set __fish_git_prompt_showcolorhints true
-#set __fish_git_prompt_show_informative_status true
-#set __fish_git_prompt_color_branch yellow
 set __fish_git_prompt_char_dirtystate '⚡'
 set __fish_git_prompt_char_untrackedfiles 'untracked'
 set __fish_git_prompt_char_cleanstate 'ok'
@@ -23,9 +21,9 @@ set __fish_git_prompt_char_invalidstate '¤'
 set __fish_git_prompt_char_stateseparator '|'
 
 set -x PIP_USE_WHEEL 'true'
-set -x PIP_WHEEL_DIR '$HOME/.pip/wheels'
-set -x PIP_FIND_LINKS '$HOME/.pip/wheels'
-set -x PIP_DOWNLOAD_CACHE '$HOME/.pip/cache'
+set -x PIP_WHEEL_DIR '/home/jwa/.config/pip/wheels'
+set -x PIP_FIND_LINKS '/home/jwa/.config/pip/wheels'
+set -x PIP_DOWNLOAD_CACHE '/home/jwa/.config/pip/cache'
 
 set -gx VIRTUALFISH_HOME $HOME/.venvs
 eval (python -m virtualfish auto_activation)
@@ -34,9 +32,11 @@ eval (direnv hook fish)
 set -x GPGKEY 29D2D3731CC0A5F2
 set -x GPG_TTY (tty)
 set -x EDITOR nvim
+set -gx PATH $PATH ~/Scripts ~/.local/bin
+set -x SHELL /usr/bin/fish
 
 # curl -sfL https://git.io/fundle-install | fish
 fundle plugin 'tuvistavie/fish-ssh-agent'
-fundle plugin 'tuvistavie/fish-fastdir'
+#fundle plugin 'fisherman/pipenv'
 fundle init
 
--- Makefile
+++ Makefile
@@ -1,10 +1,25 @@
-.PHONY: help install
+.PHONY: help install dependencies
 
 help:
-	@echo "Setup symlinks for dotfiles and copy system configuration files."
-	@echo "make install"
+	@echo "install           Setup symlinks for dotfiles and copy system configuration files."
+	@echo "dependencies      Install required system packages"
+
 
 install:
 	@echo "Symlinking dotfiles..."
 	@echo "Copying system configuration files..."
+
+
+dependencies: aur
+	@echo "Installing system dependencies..."
+
+
+aur: core
+	@echo "Installing AUR package manager..."
+
+
+core:
+	@echo "Installing core packages..."
+	sudo pacman -Syy
+	sudo pacman -S --noconfirm base-devel wget
 
